b099053cc89727ca05a0add6d864b5d3aa32bbe2
ZhekaSemenov <60353234+Esemenov85@users.noreply.github.com>
2023-10-23 16:02:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,31 @@
+import xml.etree.ElementTree as ET
 import xml.dom.minidom
-file_path = '/users/esemenov/downloads/list.xml'
-#
-# def main():
-#     # используем функцию parse() для загрузки и парсинга XML файла
-#     doc = xml.dom.minidom.parse('/users/esemenov/downloads/list.xml');
-#
-#     # выводим узел документа и имя первого дочернего тега
-#     print
-#     doc.nodeName
-#     print
-#     doc.firstChild.tagName
-#
-#     # получаем список тегов XML из документа и выводим каждый
-#     expertise = doc.getElementsByTagName("expertise")
-#     print
-#     "%d expertise:" % expertise.length
-#     for skill in expertise:
-#         print
-#         skill.getAttribute("name")
-#
-#     # создаем новый тег XML и вставляем его в документ
-#     newexpertise = doc.createElement("expertise")
-#     newexpertise.setAttribute("name", "BigData")
-#     doc.firstChild.appendChild(newexpertise)
-#     print
-#     " "
-#
-#     expertise = doc.getElementsByTagName("expertise")
-#     print
-#     "%d expertise:" % expertise.length
-#     for skill in expertise:
-#         print
-#         skill.getAttribute("name")
-#
-#
-# if name == "__main__":
-#     main();
+file_path_m = '/users/esemenov/downloads/list.xml'
+file_path_w = 'C:/Users/Evgeniy_S/Downloads/list.xml'
+
+root = ET.parse(file_path_w).getroot()
 
 # используем функцию parse() для загрузки и парсинга XML файла
-doc = xml.dom.minidom.parse(file_path);
-
+doc = xml.dom.minidom.parse(file_path_w);
 # выводим узел документа и имя первого дочернего тега
 print(doc.nodeName)
 print(doc.firstChild.tagName)
-print(doc.attributes)
 # получаем список тегов XML из документа и выводим каждый
 expertise = doc.getElementsByTagName("title")
+ch = int(expertise.length) - 1
+
+
+for child in root:
+    child.text
+
+first_n = child[0].findtext("title")
+last_n = child[122].findtext("title")
+print("первое значение:", first_n+'\n'+'последнее значение:', last_n+'\n')
+
 print("%d title:" % expertise.length)
-for skill in expertise:
-    print(skill.getAttribute(">"))
+
+n =+ 1
+i = 0
+while i <= ch:
+    print(child[i].findtext("title"))    # применяем индекс для получения элемента
+    i += 1
